7d1706c64394de8ad57ee6ebddd3c8de031c2941
kiruthikasp <kiruthikait93@gmail.com>
2016-01-23 10:11:47 +0530

    Update RingtonePickerActivity.java

--- src/android/com/adobe/phonegap/push/RingtonePickerActivity.java
+++ src/android/com/adobe/phonegap/push/RingtonePickerActivity.java
@@ -1,3 +1,15 @@
+package com.adobe.phonegap.push;
+
+import android.media.RingtoneManager;
+import android.view.View.OnClickListener;
+import android.app.Activity;
+import android.media.Ringtone;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.database.Cursor;
+import android.view.Menu;
+
 public class RingtonePickerActivity extends Activity {
 
     /** Called when the activity is first created. */
