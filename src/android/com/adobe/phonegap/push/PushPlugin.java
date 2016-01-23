adc59fe271e77e24a0afd5897f39223d241201b0
kiruthikasp <kiruthikait93@gmail.com>
2016-01-23 13:08:43 +0530

    Update PushPlugin.java

--- src/android/com/adobe/phonegap/push/PushPlugin.java
+++ src/android/com/adobe/phonegap/push/PushPlugin.java
@@ -194,9 +194,6 @@ public class PushPlugin extends CordovaPlugin implements PushConstants {
                 }
             });
         } else if (SELECT.equals(action)){
-             cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                    try {
             // Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
             // intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select ringtone for notifications:");
             // intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, false);
@@ -205,7 +202,7 @@ public class PushPlugin extends CordovaPlugin implements PushConstants {
             // this.startActivityForResult( intent, 999);  
             Context context=this.cordova.getActivity().getApplicationContext();
             //or Context context=cordova.getActiivity().getApplicationContext();
-            this.cordova.setActivityResultCallback (this); 
+            cordova.setActivityResultCallback (this); 
             Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
             intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE,RingtoneManager.TYPE_NOTIFICATION | RingtoneManager.TYPE_RINGTONE);
             intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_DEFAULT, true);
@@ -213,14 +210,6 @@ public class PushPlugin extends CordovaPlugin implements PushConstants {
             RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
             this.cordova.getActivity().startActivityForResult(intent, 999);
             callbackContext.success();
-                    } catch (UnknownError e) {
-                        callbackContext.error(e.getMessage());
-                    } catch (JSONException e) {
-                        callbackContext.error(e.getMessage());
-                    }
-            //or cordova.getActivity().startActivity(intent);
-                }
-            });
         }else {
             Log.e(LOG_TAG, "Invalid action : " + action);
             callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
