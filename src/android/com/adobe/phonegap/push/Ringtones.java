021bafdc11e801e33079990f5ed4d74fc7c1b822
kiruthikasp <kiruthikait93@gmail.com>
2016-01-23 09:44:14 +0530

    Update Ringtones.java

--- src/android/com/adobe/phonegap/push/Ringtones.java
+++ src/android/com/adobe/phonegap/push/Ringtones.java
@@ -8,6 +8,14 @@ import android.util.Log;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
+import android.view.View.OnClickListener;
+import android.app.Activity;
+import android.media.Ringtone;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.database.Cursor;
+import android.view.Menu;
 
 import com.google.android.gms.gcm.GcmPubSub;
 import com.google.android.gms.iid.InstanceID;
