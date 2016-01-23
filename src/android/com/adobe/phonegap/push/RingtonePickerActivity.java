604b7b5441386fcaf31bbca6b71ec4cefe77348f
kiruthikasp <kiruthikait93@gmail.com>
2016-01-23 10:10:42 +0530

    Create RingtonePickerActivity.java

---
+++ src/android/com/adobe/phonegap/push/RingtonePickerActivity.java
@@ -0,0 +1,9 @@
+public class RingtonePickerActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.picker);
+    }
+}
